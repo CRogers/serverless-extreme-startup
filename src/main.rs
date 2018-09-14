ff0be0570d40d1a71a11018af29390e84bee3ebd
Callum Rogers <callumrogers@hotmail.co.uk>
2018-09-14 18:34:18 +0100

    Remove reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 extern crate aws_lambda as lambda;
 #[macro_use] extern crate log;
-extern crate reqwest;
 
 use std::io::prelude::*;
-use stopwatch::Stopwatch;
 
 fn main() {
     lambda::logger::init();
     info!("New lambda started!");
 
     lambda::gateway::start(|_req| {
-        let mut response_string = String::new();
-
-        reqwest::get("https://www.googleapis.com/oauth2/v3/certs")
-            .unwrap()
-            .read_to_string(&mut response_string)
-            .expect("To be able to read");
-
         let res = lambda::gateway::response()
             .status(200)
             .body("Hello ƛ!".into())?;
