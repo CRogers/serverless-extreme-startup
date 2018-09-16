9e0ab6f4f349ae16264fd5cf2cb0ad20e469dd46
Callum Rogers <callumrogers@hotmail.co.uk>
2018-09-16 21:38:03 +0100

    Add frontend stolen from https://github.com/CRogers/mealfu/commit/c32ae96d074c8956b96ab21c85774fa53b55603a

---
+++ frontend/src/index.tsx
@@ -0,0 +1,39 @@
+import * as ReactDom from 'react-dom';
+import * as React from 'react';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from "react-redux";
+import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router'
+import createBrowserHistory from "history/createBrowserHistory";
+import { Route, Switch } from "react-router";
+import { BROWSER_URL_BASENAME } from "./constants/buildConstants";
+import { handleGithubPagesSpaRedirect } from "./github-pages/gh-pages-spa-redirect-handler";
+
+handleGithubPagesSpaRedirect();
+
+const reducer = (state:  {}) => state;
+
+const history = createBrowserHistory({
+    basename: BROWSER_URL_BASENAME
+});
+
+const store = createStore(
+    connectRouter(history)(reducer),
+    {},
+    applyMiddleware(
+        routerMiddleware(history)
+    )
+);
+
+const appContainer = document.createElement('div');
+document.body.appendChild(appContainer)
+
+ReactDom.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route exact path="/" render={() => <div>hiya!</div>}/>
+                <Route render={() => (<div>where?</div>)} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>,
+    appContainer);
---
+++ frontend/webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const webpack = require('webpack');
+const history = require('connect-history-api-fallback');
+const convert = require('koa-connect');
+
+const siteDir = path.resolve(__dirname, 'site');
+
+module.exports = (env, originalArgv) => {
+    const argv = originalArgv || {};
+    const mode = argv.mode || (process.env.WEBPACK_SERVE && 'development') || 'production';
+    console.log('In mode', mode);
+
+    return {
+        mode,
+        entry: {
+            'app': './src/index.tsx',
+            '404': './src/github-pages/gh-pages-spa-redirect.ts',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        output: {
+            filename: '[name].js',
+            path: siteDir,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Extreme Startup',
+                chunks: ['app'],
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Mealfu Github Pages SPA Redirect',
+                chunks: ['404'],
+                filename: '404.html',
+                inlineSource: '.js$'
+            }),
+            new HtmlWebpackInlineSourcePlugin(),
+            new webpack.DefinePlugin({
+                WEBPACK_DEFINED_API_URL_BASE: JSON.stringify("blah"),
+                WEBPACK_DEFINED_BROWSER_URL_BASENAME: JSON.stringify(mode === 'production'
+                    ? '/serverless-extreme-startup-frontend/'
+                    : '/')
+            })
+        ],
+        serve: {
+            open: true,
+            hot: true,
+            add: (app, middleware, options) => {
+                const historyOptions = {
+                    index: '/',
+                    rewrites: [
+                        { from: /^\/[^.]+$/, to: '/404.html' }
+                    ]
+                };
+
+                app.use(convert(history(historyOptions)));
+            },
+        }
+    };
+}
