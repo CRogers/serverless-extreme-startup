da863d458ca86badcf3f8c9d24c6c53ffc2c67c2
Callum Rogers <callumrogers@hotmail.co.uk>
2018-09-17 15:44:13 +0100

    Make button to create a new game

--- frontend/src/frontpage/frontpage.tsx
+++ frontend/src/frontpage/frontpage.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 
 export class Frontpage extends React.Component {
     public render() {
-        return <div>Hiya! Pass</div>;
+        return <div>
+            <h1>Extreme Startup!</h1>
+            <button onClick={() => alert("hi")}>Start a new game!</button>
+        </div>;
     }
 }
